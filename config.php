f13e2a24668574d11167a7aaac3ad403f88ebadb
BerothW <44261853+BerothW@users.noreply.github.com>
2019-01-11 12:54:51 +0100

    Conexión a BD

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+  $conn = new mysqli("localhost", "root", "", "lft");
+
+  if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+  }
+?>
