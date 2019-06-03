efa94ff73be267e637b59ddf6f23b667534ff610
A-Mira <albertomira97@gmail.com>
2019-06-03 23:14:12 +0200

    Carga de posts. Hay que hacer el profile y luego darle estilo.

--- includes/funciones.php
+++ includes/funciones.php
@@ -1,6 +1,19 @@
 <?php
-function loadpost($post){
+function loadpost($post){ // ID, autor, text, date
   //aquí hay que escribir todo el post.
+  $post_id = $post[0];
+  $post_autor = mysqli_query($conn, "SELECT nombre FROM usuario WHERE id like '$post[1]'");
+  $post_autor = mysqli_fetch_array($post_autor)[0];
+  $post_text = $post[2];
+  $post_date = $post[3];
 }
-
 ?>
+
+<div class="row post">
+  <div class="row author">
+    <a href="profile?p=$post_id">$post_autor</a>
+  </div>
+  <div class="row content">
+    
+  </div>
+</div>
--- profile.php
+++ profile.php
@@ -5,4 +5,10 @@
   if (!isset($_GET['p'])) {
     $_GET['p']=$self;
   }
+  $profile_id = $_GET['p']
+
+  $tabla_posts = mysqli_query($conn, "SELECT * FROM post WHERE id_autor LIKE '$profile_id' ORDER BY fecha DESC");
+  while ($post = mysqli_fetch_array($tabla_posts)) {
+    loadpost($post);
+  }
 ?>
