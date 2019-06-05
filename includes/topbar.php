8d10161cf7a91f73678b1ede2f76e7530d6b0504
A-Mira <albertomira97@gmail.com>
2019-06-05 22:12:36 +0200

    Arreglando cosillas de estilo

--- includes/topbar.php
+++ includes/topbar.php
@@ -22,8 +22,8 @@
       <div class="col m6" style="text-align: center">
         <h1 style="display: inline">LFT</h1><h3 style="display: inline">.com</h3>
       </div>
-      <div style="margin-top: 1.5%" class="col m4">
-        <a href="profile.php" style="background-color: black; margin-right: 5px" class="waves-effect waves-light btn"><i class="material-icons left">home</i>PROFILE</a>
+      <div style="margin-top: 1.5%; text-align:right" class="col m4">
+        <a href="profile.php" style="background-color: black; margin-right: 5px" class="waves-effect waves-light btn"><i class="material-icons left">account_circle</i>PROFILE</a>
         <a href="logout.php" style="background-color: black;" class="waves-effect waves-light btn"><i class="material-icons left">clear</i>LOGOUT</a>
       </div>
     </div>
